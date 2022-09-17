47bbf94a655d863390f7e6710f0d0edb0b1fa140
seanstoneburner <litholight@gmail.com>
2022-09-17 11:12:48 -0400

    initial commit

---
+++ TrinityChristianAcademy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TrinityChristianAcademy.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
